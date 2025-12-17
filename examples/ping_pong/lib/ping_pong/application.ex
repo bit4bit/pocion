c4539597d7a947b4c5aae80a4c678a0b119893d8
Jovany Leandro G.C <bit4bit@riseup.net>
2025-12-17 22:48:43 +0200

    new ping_pong

---
+++ examples/ping_pong/lib/ping_pong/application.ex
@@ -0,0 +1,18 @@
+defmodule PingPong.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      {Pocion, [:ping_pong, %{width: 640, height: 480, title: "Ping Pong", opts: [otp_app: :ping_pong]}]}
+    ]
+
+
+    opts = [strategy: :one_for_one, name: PingPong.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ examples/ping_pong/mix.exs
@@ -0,0 +1,32 @@
+defmodule PingPong.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ping_pong,
+      version: "0.1.0",
+      elixir: "~> 1.19",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      elixirc_options: [
+        warnings_as_errors: true
+      ]
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {PingPong.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:raylib, "~> 0.0", path: "../../raylib"},
+      {:pocion, "~> 0.0", path: "../../pocion"}
+    ]
+  end
+end
